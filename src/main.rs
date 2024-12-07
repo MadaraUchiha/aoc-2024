cb223c71c43e9d140bd40ef82a2e5adfa67b806f
Madara Uchiha <me@madara.ninja>
2024-12-07 11:02:05 +0200

    extract days to own module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
+mod days;
 
 mod solution;
 mod vector;
@@ -15,17 +9,18 @@ use anyhow::{anyhow, Result};
 use clap::Parser;
 use solution::Solution;
 
+use days::*;
+
 fn main() -> Result<()> {
     let cli = cli::Cli::parse();
     match cli.day {
-        1 => day01::Day01.run().unwrap(),
-        2 => day02::Day02.run().unwrap(),
-        3 => day03::Day03.run().unwrap(),
-        4 => day04::Day04.run().unwrap(),
-        5 => day05::Day05.run().unwrap(),
-        6 => day06::Day06.run().unwrap(),
-        7 => day07::Day07.run().unwrap(),
-        n => return Err(anyhow!("Day {} not implemented", n)),
+        1 => Day01.run(),
+        2 => Day02.run(),
+        3 => Day03.run(),
+        4 => Day04.run(),
+        5 => Day05.run(),
+        6 => Day06.run(),
+        7 => Day07.run(),
+        n => Err(anyhow!("Day {} not implemented", n)),
     }
-    Ok(())
 }
